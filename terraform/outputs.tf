f9ff74922f64a13d7c0cf7c2e0b8b0134e643a4a
John Duran <johalduran@gmail.com>
2025-03-23 10:45:55 -0500

    Updated

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,3 +5,8 @@ output eventbridge_rules {
 output "sns_topic_arn" {
   value = aws_sns_topic.lambda_dollar_notifications.arn
 }
+
+output prod_function_url {
+  value       = aws_lambda_function_url.prod_alias_url.function_url
+  value       = aws_lambda_function_url.dev_alias_url.function_url
+}
